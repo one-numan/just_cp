f30dc8e903c18fbf5b64c1b3bc01de16c2ffd0f5
Mohd Numan <48924562+one-numan@users.noreply.github.com>
2026-01-10 02:50:59 +0530

    Added gitignore files

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,14 +1,12 @@
 import os
 import sys
 
-
 sys.path.insert(0, os.path.dirname(__file__))
 
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "gms.settings"
+)
 
-from gms.wsgi import application
-# def application(environ, start_response):
-#     start_response('200 OK', [('Content-Type', 'text/plain')])
-#     message = 'It works!\n'
-#     version = 'Python Yoou%s\n' % sys.version.split()[0]
-#     response = '\n'.join([message, version])
-#     return [response.encode()]
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
